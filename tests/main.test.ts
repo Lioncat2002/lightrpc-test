bcc25dc660b46179f60528dc425c95c3832091e2
Lioncat2002 <sankhayan2002@gmail.com>
2022-11-15 00:45:59 +0530

    feat: started working on forward transaction test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,25 @@
-function sum(a, b) {
-    return a + b
-}
+import { Connection, Keypair, LAMPORTS_PER_SOL, Message, VersionedTransaction } from "@solana/web3.js";
+import { url } from "./urls";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('forwardTransaction', async () => {
+  const connection = new Connection(url, 'confirmed');
+  const payer = Keypair.generate();
+
+  await connection.requestAirdrop(payer.publicKey, LAMPORTS_PER_SOL);
+  const recentBlockhash = (await connection.getLatestBlockhash('confirmed')).blockhash;
+  const versionedTx = new VersionedTransaction(
+    new Message({
+      header: {
+        numRequiredSignatures: 1,
+        numReadonlySignedAccounts: 0,
+        numReadonlyUnsignedAccounts: 0,
+      },
+      recentBlockhash,
+      instructions: [],
+      accountKeys: [payer.publicKey.toBase58()],
+    }),
+  );
+
+  versionedTx.sign([payer]);
+  await connection.sendTransaction(versionedTx);
 });
