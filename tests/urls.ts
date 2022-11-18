7c141f21b3f68935c706d1e20c163a1c12a6b3f4
aniketfuryrocks <contact@aniketprajapati.me>
2022-11-18 16:26:20 +0530

    fix url; tests pass

--- tests/urls.ts
+++ tests/urls.ts
@@ -1,1 +1,4 @@
-export const url = 'https://api.testnet.solana.com';
+import { clusterApiUrl } from "@solana/web3.js";
+
+export const url = "http://127.0.0.1:8899";
+//export const url = clusterApiUrl('devnet');
