3c3c6f99402550722d8b6c891c3c768bf4e8be23
Lioncat2002 <sankhayan2002@gmail.com>
2022-11-26 17:47:36 +0530

    chore: working on executing multiple commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::process::Command;
 
 use clap::{Parser, Subcommand};
+use run_script::ScriptOptions;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -16,9 +17,21 @@ enum LiteCommand {
 }
 
 fn run_test() {
-    let mut binding = Command::new("solana-test-validator");
-    //let res = binding.args(["./lite-rpc", "yarn", "run", "test"]);
-    println!("{:?}", binding.output());
+    let mut options = ScriptOptions::new();
+    options.runner = Some("bash".to_string());
+    options.capture_output = true; // True to capture and return the output. False will print it to the parent process output.
+    options.exit_on_error = true; // Adds set -e option (not available for windows)
+    options.print_commands = false; // Adds set -x option (not available for windows)
+
+    let (code, stdout, stderr) = run_script::run_script!(
+        r#"
+        yarn run test & solana-test-validator
+        "#,
+        &vec![],
+        &options
+    )
+    .unwrap();
+    println!("code: {}, stdout: {}, stderr: {}", code, stdout, stderr);
 }
 
 fn main() {
